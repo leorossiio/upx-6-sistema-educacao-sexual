0190b2f0b7a09ca238a3897f56cc72927c85f046
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-09-25 20:15:09 -0300

    correção commit anterior

---
+++ src/models/userModel.js
@@ -0,0 +1,10 @@
+//Nome string
+//email string -> será o login
+//Senha string cript
+//Funcao string-> adm e medico
+//CRM long int podendo ser null caso nao for medico
+//Celular long int
+//Status string (Ativo ou Inativo) -> Vão ser criados com status Inativo e serão aprovados pelo ADM, onde terá a validação do CRM 
+
+// Site de validação:
+// https://portal.cfm.org.br/busca-medicos
