a7ed8a8c8b8bf8dda865e47cfb16702dabc0afa6
Leonardo Rossi <leonardo.oliveira@itixti.com.br>
2024-09-25 20:14:34 -0300

    userModel ideia

---
+++ src/models/users.js
@@ -0,0 +1,10 @@
+//Nome
+//email -> será o login
+//Senha cript
+//Funcao -> adm e medico
+//CRM podendo ser null caso nao for medico
+//Celular
+//Status (Ativo ou Inativo) -> Vão ser criados com status Inativo e serão aprovados pelo ADM, onde terá a validação do CRM 
+
+// Site de validação:
+// https://portal.cfm.org.br/busca-medicos
